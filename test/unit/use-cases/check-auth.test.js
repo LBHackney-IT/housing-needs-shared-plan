fe2f8ecc23a8c4f68aa1902e4910a0dbee325e89
Matt Millar <matthew.millar@madetech.com>
2020-05-15 15:42:57 +0100

    Update check-auth to use jwt verify

--- test/unit/use-cases/check-auth.test.js
+++ test/unit/use-cases/check-auth.test.js
@@ -1,19 +1,21 @@
 import CheckAuth from '../../../lib/use-cases/check-auth';
 
 describe('Check Auth Use Case', () => {
-  it('should decode a token', () => {
-    const jwt = { decode: jest.fn(() => ({ groups: [] })) };
+  it('should verify a token', () => {
+    const jwt = { verify: jest.fn(() => ({ groups: [] })) };
     const token = 'xyz';
+    const secret = 'secret';
+    process.env.JWT_SECRET = secret;
     const checkAuth = new CheckAuth({ allowedGroups: [], jwt });
 
     checkAuth.execute({ token });
 
-    expect(jwt.decode).toHaveBeenCalledWith(token);
+    expect(jwt.verify).toHaveBeenCalledWith(token, secret);
   });
 
   it('should return true if a user is in the allowed groups', () => {
     const allowedGroups = ['theGroup'];
-    const jwt = { decode: jest.fn(() => ({ groups: ['theGroup'] })) };
+    const jwt = { verify: jest.fn(() => ({ groups: ['theGroup'] })) };
     const checkAuth = new CheckAuth({ allowedGroups, jwt });
 
     const result = checkAuth.execute({ token: 'xyz' });
@@ -23,7 +25,7 @@ describe('Check Auth Use Case', () => {
 
   it('should return false if a user in not in the allowed groups', () => {
     const allowedGroups = ['theGroup'];
-    const jwt = { decode: jest.fn(() => ({ groups: ['otherGroup'] })) };
+    const jwt = { verify: jest.fn(() => ({ groups: ['otherGroup'] })) };
     const checkAuth = new CheckAuth({ allowedGroups, jwt });
 
     const result = checkAuth.execute({ token: 'xyz' });
@@ -34,7 +36,7 @@ describe('Check Auth Use Case', () => {
   it('a user can have more than one group', () => {
     const allowedGroups = ['firstGroup'];
     const jwt = {
-      decode: jest.fn(() => ({ groups: ['firstGroup', 'secondGroup'] })),
+      verify: jest.fn(() => ({ groups: ['firstGroup', 'secondGroup'] }))
     };
     const checkAuth = new CheckAuth({ allowedGroups, jwt });
 
@@ -43,8 +45,8 @@ describe('Check Auth Use Case', () => {
     expect(result).toBe(true);
   });
 
-  it('should return false if decoded token is null', () => {
-    const jwt = { decode: jest.fn(() => null) };
+  it('should return false if verified token is null', () => {
+    const jwt = { verify: jest.fn(() => null) };
     const checkAuth = new CheckAuth({ allowedGroups: [], jwt });
 
     const result = checkAuth.execute({ token: 'xyz' });
@@ -52,11 +54,24 @@ describe('Check Auth Use Case', () => {
     expect(result).toBe(false);
   });
 
-  it('should return false if decoded token does not contain groups', () => {
-    const jwt = { decode: jest.fn(() => ({})) };
+  it('should return false if verified token does not contain groups', () => {
+    const jwt = { verify: jest.fn(() => ({})) };
     const checkAuth = new CheckAuth({ allowedGroups: [], jwt });
 
     const result = checkAuth.execute({ token: 'xyz' });
+
+    expect(result).toBe(false);
+  });
+
+  it('should return false if token verification fails', () => {
+    const jwt = {
+      verify: jest.fn(() => {
+        throw new Error('NO!');
+      })
+    };
+    const checkAuth = new CheckAuth({ allowedGroups: [], jwt });
+
+    const result = checkAuth.execute({ token: 'WRONG' });
 
     expect(result).toBe(false);
   });
