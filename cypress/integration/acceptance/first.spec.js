58f163ecb1ae189daf42b2f15c17eaa7651def1e
Bogdan <bogdan@madetech.com>
2020-05-19 12:32:56 +0100

    checking that axe is working for individual page elements

--- cypress/integration/acceptance/first.spec.js
+++ cypress/integration/acceptance/first.spec.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress" />
-
 context('Home page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
@@ -9,7 +8,7 @@ context('Home page', () => {
   describe('Loads page', () => {
     it('has shared plan heading', () => {
       cy.get('h1').should('have.text', 'Welcome to Shared Plan!');
-      cy.checkA11y(null, null, cy.terminalLog);
+      cy.checkA11y('#content > h1', null, cy.terminalLog);
     });
   });
 });
