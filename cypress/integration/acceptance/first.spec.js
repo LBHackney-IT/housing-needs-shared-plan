504fcaca03a4078d006c3668e9b659520ab238bf
Bogdan <bogdan@madetech.com>
2020-05-19 11:07:02 +0100

    adding axe integration with cypress for accessibility testing

--- cypress/integration/acceptance/first.spec.js
+++ cypress/integration/acceptance/first.spec.js
@@ -2,13 +2,14 @@
 
 context('Home page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
-  })
+    cy.visit('http://localhost:3000/');
+    cy.injectAxe();
+  });
 
   describe('Loads page', () => {
     it('has shared plan heading', () => {
-      cy.get('h1')
-        .should('have.text', 'Welcome to Shared Plan!')
-    })
-  })
-})
+      cy.get('h1').should('have.text', 'Welcome to Shared Plan!');
+      cy.checkA11y(null, null, cy.terminalLog);
+    });
+  });
+});
