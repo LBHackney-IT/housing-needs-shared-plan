a075f8ac64c4eaece225493453148ac1178a80c9
Bogdan <bogdan@madetech.com>
2020-05-21 10:55:56 +0100

    Change agreed date and review date to account for different scenarios and remove duplication of Goal

--- test/unit/domain/goal.test.js
+++ test/unit/domain/goal.test.js
@@ -1,6 +1,5 @@
 import Goal from '../../../lib/domain/goal';
 
-
 describe('Goal', () => {
   it('sets the agreed date to todays date', () => {
     const today = new Date();
@@ -17,6 +16,14 @@ describe('Goal', () => {
     expect(goal.agreedDate).toEqual(today.toISOString());
   });
 
+  it('does not set the review date if previously set', () => {
+    const agreedDate = '2030-02-12T00:00:00.000Z';
+    const targetReviewDate = '2040-02-12T00:00:00.000Z'
+    const goal = new Goal({ agreedDate, targetReviewDate });
+
+    expect(goal.agreedDate).toEqual('2030-02-12T00:00:00.000Z');
+  });
+
   it('sets the target review date', () => {
     const targetReviewDate = {
       day: 12,
@@ -24,6 +31,13 @@ describe('Goal', () => {
       year: 2030
     };
 
+    const goal = new Goal({ targetReviewDate });
+
+    expect(goal.targetReviewDate).toEqual('2030-02-12T00:00:00.000Z');
+  });
+
+  it('does not set the target review date if previously set', () => {
+    const targetReviewDate = '2030-02-12T00:00:00.000Z';
     const goal = new Goal({ targetReviewDate });
 
     expect(goal.targetReviewDate).toEqual('2030-02-12T00:00:00.000Z');
--- test/unit/domain/plan.test.js
+++ test/unit/domain/plan.test.js
@@ -1,7 +1,7 @@
 import Plan from '../../../lib/domain/plan';
 
 describe('Plan', () => {
-  it('sets the created date to the current date/time', async () => {
+  it('sets the created date to the current date/time when no date is received', async () => {
     jest
       .spyOn(global.Date, 'now')
       .mockImplementationOnce(() =>
@@ -11,5 +11,12 @@ describe('Plan', () => {
     const plan = new Plan({});
 
     expect(plan.created).toEqual('2020-05-14T12:01:58.000Z');
+  });
+
+  it('sets the created date to the date received', () => {
+    const created = '2019-12-17T00:00:00';
+    const plan = new Plan({ created });
+
+    expect(plan.created).toEqual(created);
   });
 });
