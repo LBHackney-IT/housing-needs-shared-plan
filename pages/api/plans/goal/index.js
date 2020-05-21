a765b93956efe023c171af4f8fa062e26eb7c2a1
Bogdan <bogdan@madetech.com>
2020-05-21 16:14:15 +0100

    Add api endpoint for add goal use case

---
+++ pages/api/plans/goal/index.js
@@ -0,0 +1,26 @@
+import { addGoal } from '../../../../lib/dependencies';
+import { ArgumentError } from '../../../../lib/domain';
+import { logger } from '../../../../lib/infrastructure/logging';
+
+export const endpoint = ({ addGoal }) => async (req, res) => {
+  try {
+    const result = await addGoal({
+      planId: req.body.planId,
+      goal: req.body.goal
+    });
+
+    logger.info(`Success`, { result });
+    res.status(200).json(result);
+  } catch (err) {
+    logger.error(err.message, { err });
+    if (err instanceof ArgumentError) {
+      return res.status(400).json({ error: `could not add goal to plan` });
+    }
+
+    res.status(500).json({
+      error: `could not add goal to plan with id=${req.body.planId}`
+    });
+  }
+};
+
+export default endpoint({ addGoal });
