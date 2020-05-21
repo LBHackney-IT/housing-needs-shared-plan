2a20a7aa013f017f863fe09f0098d69ccd2c43f1
Kat <katrina@madetech.com>
2020-05-21 16:51:55 +0100

    Add add-action api end point and update jest config to reset the mocks

---
+++ test/unit/pages/api/add-action.test.js
@@ -0,0 +1,42 @@
+import { endpoint } from 'pages/api/plans/[id]/actions';
+
+describe('Add action API endpoint', () => {
+  let res;
+  let json;
+
+  beforeEach(function() {
+    json = jest.fn();
+    res = {
+      status: jest.fn(() => {
+        return { json };
+      })
+    };
+  });
+
+  const req = {
+    query: {
+      id: '1'
+    },
+    body: {
+      summary: 'test',
+      description: 'test Description',
+      dueDate: new Date()
+    }
+  };
+
+  it('can add an action', async () => {
+    const addAction = jest.fn();
+    await endpoint({ addAction })(req, res);
+
+    expect(addAction).toHaveBeenCalledWith({ planId: '1', action: req.body });
+    expect(res.status).toHaveBeenCalledWith(201);
+  });
+
+  it('returns 500 if there is an error adding the action', async () => {
+    const addAction = jest.fn(() => {
+      throw new Error('error');
+    });
+    await endpoint({ addAction })(req, res);
+    expect(res.status).toHaveBeenCalledWith(500);
+  });
+});
