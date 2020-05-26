b0df97eda48c1ad797d561151dca00d848bb5f16
Kat <katrina@madetech.com>
2020-05-26 09:45:32 +0100

    Started writing AddAction onClick WIP

--- components/AddAction/AddActionForm/index.js
+++ components/AddAction/AddActionForm/index.js
@@ -3,15 +3,32 @@ import SummaryTextBox from './SummaryTextBox';
 import DescriptionTextArea from './DescriptionTextArea';
 import DateInput from './DateInput';
 
+const AddAction = async () => {
+  const plan = {};
+
+  const response = await fetch(
+    `${process.env.SHARED_PLAN_API_URL}/plans/${query.id}/actions`,
+    {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(plan)
+    }
+  );
+  return response.json();
+};
 const AddActionForm = () => (
   <p class="govuk-body">
     <SummaryTextBox />
     <DescriptionTextArea />
     <DateInput title="Due date" />
+
     <button
-      disabled="disabled"
-      class="govuk-button govuk-button--disabled"
+      class="govuk-button"
       data-module="govuk-button"
+      onClick={AddAction()}
     >
       Add to plan
     </button>
