01289e4d620db0ed8ed0d028264ce9bb4d8060c4
Kat <katrina@madetech.com>
2020-05-28 11:37:05 +0100

    Add api endpoint for share plan

---
+++ pages/api/plans/share/index.js
@@ -0,0 +1,26 @@
+import { sharePlan } from 'lib/dependencies';
+import { logger } from 'lib/infrastructure/logging';
+import { ArgumentError } from 'lib/domain';
+
+export const endpoint = ({ sharePlan }) => async (req, res) => {
+  try {
+    const result = await sharePlan.execute({
+      planId: req.body.planId
+    });
+
+    logger.info(`Success`, { result });
+
+    res.status(200).json(result);
+  } catch (err) {
+    logger.error(err.message, { err });
+    if (err instanceof ArgumentError) {
+      return res.status(400).json({ error: `could not share a plan` });
+    }
+
+    res.status(500).json({
+      error: `could not share a plan with plan id=${req.body.planId}`
+    });
+  }
+};
+
+export default endpoint({ sharePlan });
---
+++ test/unit/pages/api/plans/share/index.test.js
@@ -0,0 +1,75 @@
+import { endpoint } from 'pages/api/plans/share';
+import { ArgumentError } from '../../../../../../lib/domain';
+
+describe('Share plan', () => {
+  const planId = '2';
+
+  let json;
+  let res;
+
+  beforeEach(() => {
+    json = jest.fn();
+    res = {
+      status: jest.fn(() => {
+        return { json };
+      })
+    };
+  });
+
+  const req = {
+    method: 'GET',
+    body: {
+      planId
+    }
+  };
+
+  it('can share a plan', async () => {
+    const planToken = 'random_string';
+    const expectedResponse = expect.objectContaining({ planToken });
+
+    const sharePlan = {
+      execute: jest.fn(() => expectedResponse)
+    };
+
+    await endpoint({ sharePlan })(req, res);
+
+    expect(sharePlan.execute).toHaveBeenCalledWith({ planId });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(json).toHaveBeenCalledWith(expectedResponse);
+  });
+
+  it('handles bad requests', async () => {
+    const expectedResponse = { error: 'could not share a plan' };
+
+    const sharePlan = {
+      execute: jest.fn()
+    };
+
+    sharePlan.execute.mockImplementation(() => {
+      throw new ArgumentError('something is missing');
+    });
+
+    await endpoint({ sharePlan })(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(json).toHaveBeenCalledWith(expectedResponse);
+  });
+
+  it('handles general errors', async () => {
+    const expectedResponse = {
+      error: `could not share a plan with plan id=${planId}`
+    };
+
+    const sharePlan = {
+      execute: jest.fn()
+    };
+    sharePlan.execute.mockImplementation(() => {
+      throw new Error('error!');
+    });
+
+    await endpoint({ sharePlan })(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(json).toHaveBeenCalledWith(expectedResponse);
+  });
+});
