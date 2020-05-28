375605f2e42b58da60972f97e15b04ed7effc468
Kat <katrina@madetech.com>
2020-05-28 14:25:18 +0100

    Extract token as domain object and refactor tests

--- test/unit/use-cases/share-plan.test.js
+++ test/unit/use-cases/share-plan.test.js
@@ -1,24 +1,35 @@
 import SharePlan from 'lib/use-cases/share-plan';
+import { Token } from 'lib/domain';
 
 describe('Share plan', () => {
+  const planId = 10;
+  const name = 'Jon';
+  const number = '00000';
+
+  const tokens = [
+    new Token({
+      token: 'random_string',
+      createdDate: expect.stringContaining(
+        new Date().toISOString().substring(0, 10)
+      )
+    })
+  ];
+
+  const plan = {
+    name,
+    number,
+    planId,
+    tokens
+  };
+
   const sharePlanRequest = {
-    planId: '10',
-    name: 'Jon',
-    number: '0000000'
+    planId,
+    name,
+    number
   };
 
   const expectedGatewayRequest = {
-    name: 'Jon',
-    number: '0000000',
-    planId: '10',
-    tokens: [
-      {
-        token: 'random_string',
-        createdDate: expect.stringContaining(
-          new Date().toISOString().substring(0, 10)
-        )
-      }
-    ]
+    plan
   };
 
   const oldToken = 'old_token';
@@ -50,11 +61,11 @@ describe('Share plan', () => {
   it('adds a new token if one already exists', async () => {
     sharePlanRequest.tokens = [{ token: oldToken, createdDate: oldDate }];
 
-    expectedGatewayRequest.tokens.push({
+    expectedGatewayRequest.plan.tokens.push({
       token: oldToken,
       createdDate: expect.stringContaining(oldDate.substring(0, 10))
     });
-    expectedGatewayRequest.tokens.reverse();
+    expectedGatewayRequest.plan.tokens.reverse();
 
     const planGateway = { save: jest.fn() };
     const smsGateway = { sendMessage: jest.fn() };
@@ -69,9 +80,11 @@ describe('Share plan', () => {
 
   it('shares the plan via sms', async () => {
     const expectedSmsRequest = {
-      message: `You’ve been sent a link to your Shared Plan from Hackney Council. Click here to view: ${process.env.SHARED_PLAN_URL}/customers/${sharePlanRequest.planId}/plan#token=random_string`,
-      name: 'Jon',
-      number: '0000000'
+      plan: {
+        message: `You’ve been sent a link to your Shared Plan from Hackney Council. Click here to view: ${process.env.SHARED_PLAN_URL}/customers/${sharePlanRequest.planId}/plan#token=random_string`,
+        name: 'Jon',
+        number: '0000000'
+      }
     };
     const planGateway = { save: jest.fn() };
     const smsGateway = { sendMessage: jest.fn() };
