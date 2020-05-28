4d5e9a1a0c1649346a5ae28eb09cb3199347f6eb
Matt Millar <matthew.millar@madetech.com>
2020-05-28 19:42:06 +0100

    Add panel component

--- components/Form/index.js
+++ components/Form/index.js
@@ -3,5 +3,6 @@ import Checkbox from './Checkbox';
 import DateInput from './DateInput';
 import SummaryList from './SummaryList';
 import TextInput from './TextInput';
+import Panel from './Panel';
 
-export { Button, Checkbox, DateInput, SummaryList, TextInput };
+export { Button, Checkbox, DateInput, SummaryList, TextInput, Panel };
