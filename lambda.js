2fd6f042f96c47dc41b66a95e32e6203bc050e75
Kat <katrina@madetech.com>
2020-06-05 11:03:40 +0100

    Add check customer token use case

--- lambda.js
+++ lambda.js
@@ -4,6 +4,7 @@ const files = require('serve-static');
 const path = require('path');
 const nextRequestHandler = app.getRequestHandler();
 const CheckAuth = require('./lib/use-cases/check-auth');
+const { checkCustomerToken } = require('lib/dependencies');
 
 server.use(require('cookie-parser')());
 server.use(files(path.join(__dirname, 'build')));
@@ -11,6 +12,14 @@ server.use(files(path.join(__dirname, 'public')));
 
 // api routes, auth is handled by the authorizer
 server.all('/api/*', (req, res) => nextRequestHandler(req, res));
+
+const authorizeCustomerHandler = (req, res, next) => {
+  const planId = req.path.split('/')[1];
+  const token = req.queryStringParameters?.token : null;
+  if (checkCustomerToken({ planId, token })) {
+  }
+  next();
+};
 
 const authoriseHandler = (req, res, next) => {
   const checkAuth = new CheckAuth({
@@ -26,6 +35,13 @@ const authoriseHandler = (req, res, next) => {
   }
   next();
 };
+
+server.all(
+  '/plans/:id',
+  (req, res, next) => authorizeCustomerHandler(req, res, next),
+  (req, res, next) => authoriseHandler(req, res, next), // everything else we need to check the cookie,
+  (req, res) => nextRequestHandler(req, res)
+);
 
 server.all(
   '*',
