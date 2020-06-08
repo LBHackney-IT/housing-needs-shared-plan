f911e7974bb4af174a6a5b9adbb516cbfe13f578
Tom Davies <tom.davies@madetech.com>
2020-06-08 14:43:37 +0100

    Refactors error handling inside of usePlan

--- api/usePlan.js
+++ api/usePlan.js
@@ -14,44 +14,42 @@ export function usePlan(planId, { initialPlan, token, ...options } = {}) {
     ...options
   });
 
+  function withErrorHandling(fn) {
+    return async (...args) => {
+      try {
+        await fn(...args);
+      } catch (err) {
+        setError(err);
+      }
+    };
+  }
+
   return {
     plan: data,
     error,
     loading: data === null,
-    addGoal: async goal => {
-      try {
-        await requestAddGoal(planId, goal, { token });
-        mutate({ ...data, goal });
-      } catch (err) {
-        setError(err);
-      }
-    },
-    addAction: async action => {
-      try {
-        await requestAddAction(planId, action, { token });
-        mutate({
-          ...data,
-          goal: {
-            ...data.goal,
-            actions: data.goal.actions.concat(action)
-          }
-        });
-      } catch (err) {
-        setError(err);
-      }
-    },
-    sharePlan: async collaborator => {
-      try {
-        const newToken = await requestSharePlan(planId, collaborator, {
-          token
-        });
-        mutate({
-          ...data,
-          customerTokens: data.customerTokens.concat([newToken])
-        });
-      } catch (err) {
-        setError(err);
-      }
-    }
+    addGoal: withErrorHandling(async goal => {
+      await requestAddGoal(planId, goal, { token });
+      mutate({ ...data, goal });
+    }),
+    addAction: withErrorHandling(async action => {
+      await requestAddAction(planId, action, { token });
+      mutate({
+        ...data,
+        goal: {
+          ...data.goal,
+          actions: data.goal.actions.concat(action)
+        }
+      });
+    }),
+    sharePlan: withErrorHandling(async collaborator => {
+      const newToken = await requestSharePlan(planId, collaborator, {
+        token
+      });
+      mutate({
+        ...data,
+        customerTokens: data.customerTokens.concat([newToken])
+      });
+    })
   };
 }
