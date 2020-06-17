8f82806c59ea981af946a22900ae02dc6ae0f4c2
Bogdan <bogdan@madetech.com>
2020-06-17 15:47:01 +0100

    Refactor cookie banner

--- components/Feature/CookieBanner/index.js
+++ components/Feature/CookieBanner/index.js
@@ -1,118 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import ButtonGroup from '../../Form/ButtonGroup';
-
-const deleteCookies = () => {
-  document.cookie = '_ga=; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/;';
-  document.cookie = '_gid=; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/;';
-  document.cookie =
-    '_gat_UA-168604600-1=; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/;';
-};
+import {
+  acceptedCookieIsSet,
+  setAcceptedCookie
+} from 'lib/utils/acceptCookies';
 
 const CookieBanner = () => {
-  const [cookieState, setCookieState] = useState('unread');
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
-    const cookies = document.cookie.split(';');
-    const cookiesPreferencesSet = cookies
-      .find(c => c.trim().startsWith('cookies_preferences_set'))
-      ?.split('=')?.[1];
-
-    if (cookiesPreferencesSet === 'true') {
-      setCookieState('set');
-    } else {
-      setCookieState('unset');
-    }
-  }, []);
+    const cookieIsSet = acceptedCookieIsSet(document.cookie);
+    setShow(!cookieIsSet);
+  });
 
   const handleAccept = () => {
-    const today = new Date(),
-      [year, month, day] = [
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate()
-      ],
-      cookieExpiryDate = new Date(year + 1, month, day).toUTCString();
-
-    document.cookie = `cookies_preferences_set=true; expires=${cookieExpiryDate};`;
-    document.cookie = `cookies_accepted=true; expires=${cookieExpiryDate};`;
+    setAcceptedCookie(true);
     window['ga-disable-UA-168604600-1'] = false;
-    setCookieState('set');
+    setShow(false);
   };
 
   const handleDecline = () => {
-    const today = new Date(),
-      [year, month, day] = [
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate()
-      ],
-      cookieExpiryDate = new Date(year + 1, month, day).toUTCString();
-    deleteCookies();
-    document.cookie = `cookies_preferences_set=true; expires=${cookieExpiryDate};`;
-    document.cookie = `cookies_accepted=false; expires=${cookieExpiryDate};`;
-    setCookieState('set');
+    setAcceptedCookie(false);
+    window['ga-disable-UA-168604600-1'] = true;
+    setShow(false);
   };
 
-  if (cookieState === 'unset') {
-    return (
-      <div
-        id="global-cookie-message"
-        className="gem-c-cookie-banner govuk-clearfix"
-        data-module="cookie-banner"
-        role="region"
-        aria-label="cookie banner"
-        data-nosnippet=""
-        style={{ display: 'block' }}
-      >
-        <div className="gem-c-cookie-banner__wrapper govuk-width-container">
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-two-thirds">
-              <div className="gem-c-cookie-banner__message">
-                <span className="govuk-heading-m">
-                  Tell us whether you accept cookies
-                </span>
-                <p className="govuk-body">
-                  We use{' '}
-                  <a
-                    href="https://hackney.gov.uk/privacy"
-                    className="govuk-link"
-                  >
-                    cookies to collect information
-                  </a>{' '}
-                  about how you use this site. We use this information to make
-                  the website work as well as possible.
-                </p>
-              </div>
-              <div className="gem-c-cookie-banner__buttons gem-c-cookie-banner__buttons--flex">
-                <ButtonGroup>
-                  <button
-                    className="gem-c-button govuk-button gem-c-button--inline"
-                    type="submit"
-                    data-module="track-click"
-                    data-accept-cookies="true"
-                    data-track-category="cookieBanner"
-                    onClick={handleAccept}
-                  >
-                    Yes
-                  </button>
-                  <button
-                    className="gem-c-button govuk-button gem-c-button--inline"
-                    type="submit"
-                    data-module="track-click"
-                    data-track-category="cookieBanner"
-                    onClick={handleDecline}
-                  >
-                    No
-                  </button>
-                </ButtonGroup>
+  return (
+    <>
+      {show && (
+        <div
+          id="global-cookie-message"
+          className="gem-c-cookie-banner govuk-clearfix"
+          data-module="cookie-banner"
+          role="region"
+          aria-label="cookie banner"
+          data-nosnippet=""
+          style={{ display: 'block' }}
+        >
+          <div className="gem-c-cookie-banner__wrapper govuk-width-container">
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-two-thirds">
+                <div className="gem-c-cookie-banner__message">
+                  <span className="govuk-heading-m">
+                    Tell us whether you accept cookies
+                  </span>
+                  <p className="govuk-body">
+                    We use{' '}
+                    <a
+                      href="https://hackney.gov.uk/privacy"
+                      className="govuk-link"
+                    >
+                      cookies to collect information
+                    </a>{' '}
+                    about how you use this site. We use this information to make
+                    the website work as well as possible.
+                  </p>
+                </div>
+                <div className="gem-c-cookie-banner__buttons gem-c-cookie-banner__buttons--flex">
+                  <ButtonGroup>
+                    <button
+                      className="gem-c-button govuk-button gem-c-button--inline"
+                      type="submit"
+                      data-module="track-click"
+                      data-accept-cookies="true"
+                      data-track-category="cookieBanner"
+                      data-testid="cookies-yes-button-test"
+                      onClick={handleAccept}
+                    >
+                      Yes
+                    </button>
+                    <button
+                      className="gem-c-button govuk-button gem-c-button--inline"
+                      type="submit"
+                      data-module="track-click"
+                      data-track-category="cookieBanner"
+                      data-testid="cookies-no-button-test"
+                      onClick={handleDecline}
+                    >
+                      No
+                    </button>
+                  </ButtonGroup>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-  return null;
+      )}
+    </>
+  );
 };
 
 export default CookieBanner;
