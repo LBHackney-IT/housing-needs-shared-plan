63fb99f03a0909b5175f05ca4a214b6c1065e855
Kat <katrina@madetech.com>
2020-08-10 14:47:37 +0100

    Remove tags

--- pages/loggedout/index.js
+++ pages/loggedout/index.js
@@ -1,7 +1,5 @@
 export default function LoggedOut() {
   return (
-    <>
-      <div>You are not currently logged in. Please login via Single View.</div>
-    </>
+    <div>You are not currently logged in. Please login via Single View.</div>
   );
 }
