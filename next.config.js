dc0da7e4d8bddea04cffa928e46a2b581d7ad70f
Charli Wild <charli.wild@charlis-mbp.lan>
2022-09-01 12:03:19 +0100

    updated syntax for ignoring build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
-    return config
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin({resourceRegExp:/.*\.test\.js$/}));
+    return config;
   },
   experimental: {
     forceSwcTransforms: true,
