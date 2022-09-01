18c7855a58586bf600692bb69d88b75e5e30a04b
Charli Wild <charli.wild@charlis-mbp.lan>
2022-09-01 10:51:17 +0100

    updated next config

---
+++ next.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  // webpack: (config, { webpack }) => {
+  //   config.plugins.push(new webpack.IgnorePlugin(/.*\.test\.js$/));
+  //   return config;
+  // },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  distDir: 'build/_next',
+  target: 'server'
+};
